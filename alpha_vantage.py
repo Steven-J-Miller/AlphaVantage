89417587eb3330a3c73a3b35552c6abe30eebc4f
Steven <mearnhardtfan@gmail.com>
2019-08-08 18:10:04 -0400

    Added Docstrings
    Added docstrings to all functions, adjusted example file to write all printed output to file instead of printing. Used for logging purposes in the intent of future script automation

--- alpha_vantage.py
+++ alpha_vantage.py
@@ -4,26 +4,34 @@ from time import sleep
 
 class AlphaVantage(object):
 
-    def __init__(self, api_key, **kwargs):
+    def __init__(self, api_key):
         super(AlphaVantage, self).__init__()
 
         self.BASE_URL = 'https://www.alphavantage.co/query?'
         self.KEY = api_key
 
-    def retrieve_data(self, path, index_col='time'):
+    @staticmethod
+    def retrieve_data(path, index_col='time'):
+        """returns data from a specified path (local or url) and returns a DataFrame
+        index_col can be specified, defaults to time if not specified.
+        12 second sleep function included to comply with AlphaVantage's limit of 5 requests/minute.
+        Only to be used internally, not intended to be called outside of the class"""
         df = pd.read_csv(path, index_col=index_col)
-        sleep(1)
+        sleep(12)
         return df
 
     def get_ma(self, symbol='SPY', ma_type='SMA', time_period=30, interval='daily', series_type='open'):
+        """Retrieves moving average technical data. Defaults to 30 day simple moving average using open prices"""
         url = f'{self.BASE_URL}function={ma_type}&symbol={symbol}&interval={interval}&' \
             f'time_period={time_period}&series_type={series_type}&apikey={self.KEY}&datatype=csv'
         try:
             return self.retrieve_data(url, index_col='time')
         except Exception as e:
             print(e)
+            return e
 
     def get_macd(self, symbol='SPY', fastperiod=12, slowperiod=26, signalperiod=9, interval='daily', series_type='open'):
+        """Retrieves MACD, defaults to 12/26/9 daily open data"""
         url = f'{self.BASE_URL}function=MACD&symbol={symbol}&interval={interval}&' \
             f'series_type={series_type}&apikey={self.KEY}&fastperiod={fastperiod}&slowperiod={slowperiod}&' \
             f'signalperiod={signalperiod}&datatype=csv'
@@ -31,16 +39,22 @@ class AlphaVantage(object):
             return self.retrieve_data(url, index_col='time')
         except Exception as e:
             print(e)
+            return e
 
     def get_rsi(self, symbol='SPY', interval='daily', time_period=60, series_type='open'):
+        """Retrieves RSI, defaults to 60 day RSI using open prices"""
         url = f'{self.BASE_URL}function=RSI&symbol={symbol}&interval={interval}&' \
             f'time_period={time_period}&series_type={series_type}&apikey={self.KEY}&datatype=csv'
         try:
             return self.retrieve_data(url, index_col='time')
         except Exception as e:
             print(e)
+            return e
 
     def get_daily_data(self, symbol='SPY', outputsize=None):
+        """Retrieves daily stock ticker open high low close and volume.
+        outputsize can be specified as full or compact. If not specified, default behavior is compact
+        compact will only retrieve the most recent 100 values while full retrieves a full history"""
         if outputsize is None:
             url = f'{self.BASE_URL}function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={self.KEY}&datatype=csv'
         elif outputsize == 'full':
@@ -49,3 +63,4 @@ class AlphaVantage(object):
             return self.retrieve_data(url, index_col='timestamp')
         except Exception as e:
             print(e)
+            return e
--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-# TODO: Add functions for CCI, AROON, BBANDS, AD, OBV
-# https://www.alphavantage.co/documentation/
-
 import pandas as pd
 import configparser
 import logging
@@ -8,30 +5,55 @@ from alpha_vantage import AlphaVantage
 from datetime import datetime
 
 
-def start_logger():
-    now = datetime.strftime(datetime.now(), '%Y-%m-%d %H%M%S %f')
-    logging.basicConfig(filename=f'logs/report_{now}.log', level=logging.DEBUG)
-
-
 def get_config(env):
+    """Retrieves any necessary configuration information from config/config.cfg"""
     config = configparser.ConfigParser()
-    if env == "DEV":
-        config.read(['config/development.cfg'])
-    elif env == "PROD":
-        config.read(['config/production.cfg'])
+    config.read(['config/config.cfg'])
     return config
 
 
+def possible_outliers(series):
+    """Identifies potential outliers in the data by identifying any values more than 5
+    standard deviations from the mean"""
+    mean = series.mean()
+    sd = series.std()
+    above_5_sd = [i for i in series if i > mean+sd*5]
+    below_5_sd = [i for i in series if i < mean-sd*5]
+    print(f'Extreme values found in {series.name}:')
+    print(above_5_sd)
+    print(below_5_sd)
+    return len(above_5_sd)+len(below_5_sd)
+
+
 if __name__ == '__main__':
+    # Start logger, overwrite print function with logger.info to record all program output to file
+    logging.basicConfig(level=logging.INFO, format='%(message)s')
+    logger = logging.getLogger()
+    now = datetime.strftime(datetime.now(), '%Y%m%d %H%M%S %f')
+    logger.addHandler(logging.FileHandler(f'{now}.log', 'a'))
+    print = logger.info
+
+    # Retrieve API key, create AlphaVantage object
     configuration = get_config('DEV')
     key = configuration.get('credentials', 'apikey')
     alpha = AlphaVantage(key)
-    daily_data = alpha.get_daily_data('FXI', 'full')
-    sma = alpha.get_ma('FXI')
-    ema = alpha.get_ma('FXI', 'EMA')
-    wma = alpha.get_ma('FXI', 'WMA')
-    macd = alpha.get_macd('FXI')
-    rsi = alpha.get_rsi('FXI')
-    adx = alpha.get_ma('FXI', 'ADX')
+
+    # Retrieve market data
+    daily_data = alpha.get_daily_data('AMD', 'full')
+    sma = alpha.get_ma('AMD')
+    ema = alpha.get_ma('AMD', 'EMA')
+    wma = alpha.get_ma('AMD', 'WMA')
+    macd = alpha.get_macd('AMD')
+    rsi = alpha.get_rsi('AMD')
+    adx = alpha.get_ma('AMD', 'ADX')
+
+    # join all frames together by index, write to file
     full_df = pd.concat([daily_data, sma, ema, wma, macd, adx, rsi], axis=1, sort=False)
     full_df.to_csv('output.csv')
+
+    # check for possible duplicate entries
+    print(f'Potential duplicate entries: {len(full_df[full_df.duplicated()])}')
+
+    # check for outliers/bad data
+    for column in full_df.columns:
+        print(possible_outliers(full_df[column]))
